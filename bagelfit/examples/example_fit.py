4e9fe71c61d48ff1986a7851e8269cda11f31144
Neelesh Soni <neeleshsoni03@gmail.com>
2025-03-06 15:51:47 -0800

    minor changes

--- bagelfit/examples/example_fit.py
+++ bagelfit/examples/example_fit.py
@@ -18,13 +18,14 @@ data_path = os.path.join(script_dir,"./yeast_membrane/")
 #-----------------------------------------------------#
 fitter= bf.BagelFitter()
 
-tor_R = 670; tor_r=160; tor_th=85; extension=0.0
+tor_R = 660; tor_r=140; tor_th=55; extension=0.0
 best_torus = fitter.generate_binary_torus(
 	tor_R, tor_r, tor_th, 
 	extension=0.0, 
 	boundingbox_length=2240, 
 	voxel_size=10.0, 
 	outmap_fname=os.path.join(data_path,"torus_yeast_fitted.mrc" ))
+
 
 #-----------------------------------------------------#
 # Score a torus map with other torus maps or experimental maps
@@ -46,7 +47,7 @@ fitter= bf.BagelFitter()
 #Below map has 11 million voxels
 fitter.load_exprimental_map(os.path.join(data_path,"Yeast_C8_Double_MR_center.mrc" ))
 
-tor_R_range=(670, 680, 10); tor_r_range=(160, 180, 20); tor_th_range=(85, 95, 10)
+tor_R_range=(660, 670, 10); tor_r_range=(140, 160, 20); tor_th_range=(55, 65, 10)
 extension = 0.0
 
 best_torus = fitter.fit_binary_torus(tor_R_range, tor_r_range, tor_th_range, extension)
